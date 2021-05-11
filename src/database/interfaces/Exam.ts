be31c6c4c7d8ea24648b524bb2c94b24a9f01a30
Erick Gannem <erickgannem@gmail.com>
2021-05-11 11:09:26 -0400

    create Exam model and interface

---
+++ src/database/interfaces/Exam.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+import Subject from './Subject'
+import Question from './Question'
+
+export default interface Exam extends mongoose.Document {
+  subject: {
+    type: Subject,
+    ref: string
+  },
+  questions: {
+    type: Question[],
+    ref: string
+  }
+}
---
+++ src/database/models/Exam.ts
@@ -0,0 +1,18 @@
+import { Schema, model } from 'mongoose'
+
+import Exam from '../interfaces/Exam'
+
+const { Types } = Schema
+const { ObjectId } = Types
+
+const examSchema = new Schema({
+  subject: {
+    type: ObjectId,
+    ref: 'Subject'
+  },
+  questions: [
+    { type: ObjectId, ref: 'Question' }
+  ]
+})
+
+export default model<Exam>('Exam', examSchema)
